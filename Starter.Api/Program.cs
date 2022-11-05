f61ae8de3db178712d2ba62492ee3f350cd39833
Lieven Vernaillen <lieven.vernaillen@gmail.com>
2022-11-05 21:48:28 +0100

    Move to minimal api

--- Starter.Api/Program.cs
+++ Starter.Api/Program.cs
@@ -1,13 +1,65 @@
-namespace Starter.Api;
+using Starter.Api.Requests;
+using Starter.Api.Responses;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+app.UseHttpsRedirection();
+
+/// <summary>
+/// This request will be made periodically to retrieve information about your Battlesnake,
+/// including its display options, author, etc.
+/// </summary>
+app.MapGet("/", () =>
 {
-    public static void Main(string[] args)
+    return new InitResponse
     {
-        CreateHostBuilder(args).Build().Run();
-    }
+        ApiVersion = "1",
+        Author = "",
+        Color = "#FFFFFF",
+        Head = "default",
+        Tail = "default"
+    };
+});
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-}
+/// <summary>
+/// Your Battlesnake will receive this request when it has been entered into a new game.
+/// Every game has a unique ID that can be used to allocate resources or data you may need.
+/// Your response to this request will be ignored.
+/// </summary>
+app.MapPost("/start", (GameStatusRequest gameStatusRequest) =>
+{
+    Results.Ok();
+});
+
+/// <summary>
+/// This request will be sent for every turn of the game.
+/// Use the information provided to determine how your
+/// Battlesnake will move on that turn, either up, down, left, or right.
+/// </summary>
+app.MapPost("/move", (GameStatusRequest gameStatusRequest) =>
+{
+    var direction = new List<string> { "down", "left", "right", "up" };
+    var rng = new Random();
+
+    return new MoveResponse
+    {
+        Move = direction[rng.Next(direction.Count)],
+        Shout = "I am moving!"
+    };
+});
+
+/// <summary>
+/// Your Battlesnake will receive this request whenever a game it was playing has ended.
+/// Use it to learn how your Battlesnake won or lost and deallocated any server-side resources.
+/// Your response to this request will be ignored.
+/// </summary>
+app.MapPost("/end", (GameStatusRequest gameStatusRequest) =>
+{
+    Results.Ok();
+});
+
+app.Run();
